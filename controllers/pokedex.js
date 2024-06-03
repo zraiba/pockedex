31e22aa3e5ec9235f173b86f36fdaa94ad860160
Youssef Zraiba <y.zraiba@studenti.unibg.it>
2024-06-04 00:50:11 +0200

    Create API entry points with router and controller setup

---
+++ controllers/pokedex.js
@@ -0,0 +1,14 @@
+
+  const PokedexController = {
+    getPokemonInformation: (req, res) => {
+      //TODO: to be implemented
+      res.json({ message: `Method to be implemented` });
+    },
+  
+    getTransaltedPokemonDescription: (req, res) => {
+      //TODO: To be implemented
+      res.json({ message: `Method to be implemented` });
+    }
+  };
+  
+  module.exports = { PokedexController };
---
+++ routes/pokedex.js
@@ -0,0 +1,10 @@
+// src/routes/pokedex.js
+const { Router } = require("express");
+const { PokedexController } = require("../controllers/pokedex.js");
+
+const router = new Router();
+
+router.get("/api/v1/pokemon/:pokemonName", PokedexController.getPokemonInformation);
+router.get("/api/v1/pokemon/translated/:pokemonName", PokedexController.getTransaltedPokemonDescription);
+
+module.exports = { router };
