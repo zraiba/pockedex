1f7c0e5458b611a168b443d50c95e03597bd870d
zraiba <yzraibaing@gmail.com>
2024-06-05 20:03:09 +0200

    feature: getTransaltedPokemonDescription and getPokemonInformation refactor

--- controllers/pokedex.js
+++ controllers/pokedex.js
@@ -1,20 +1,82 @@
-const getFunTranslation = require("../apiClients/funTranslationApiClient.js");
-const getPokemonInformation = require("../apiClients/pokeApiClient.js");
-const utils = require("../utils/utils.js");
+const FunTranslatorApi = require("../apiClients/funTranslationApiClient.js");
+const PokeApi = require("../apiClients/pokeApiClient.js");
+const { filterPokemonFields } = require("../utils/utils.js");
 
 const PokedexController = {
 	getPokemonInformation: async (req, res) => {
-		const pokemonName = req.params.pokemonName;
-		const pokeInfo = await getPokemonInformation.getPokemonInformation(
-			`pokemon-species/${pokemonName}`
-		);
+		const { pokemonName } = req.params;
 
-		const pokemonData = utils.filterPokemonFields(pokeInfo);
-		res.json(pokemonData);
+		if (!pokemonName) {
+			return res
+				.status(400)
+				.json({ error: "Pokemon name parameter is required" });
+		}
+
+		try {
+			const pokeInfo = await PokeApi.getPokemonInformation(
+				`pokemon-species/${pokemonName}`
+			);
+			const pokemonData = filterPokemonFields(pokeInfo);
+
+			res.json(pokemonData);
+		} catch (error) {
+			console.error(`Error fetching Pokemon information: ${error.message}`);
+			res.status(500).json({ error: "Failed to fetch Pokemon information" });
+		}
 	},
 
-	getTransaltedPokemonDescription: (req, res) => {
-		res.json({ message: "Method to be implemented" });
+	getTranslatedPokemonDescription: async (req, res) => {
+		const { pokemonName } = req.params;
+
+		if (!pokemonName) {
+			return res
+				.status(400)
+				.json({ error: "Pokemon name parameter is required" });
+		}
+
+		try {
+			const pokeInfo = await PokeApi.getPokemonInformation(
+				`pokemon-species/${pokemonName}`
+			);
+			const pokemonData = filterPokemonFields(pokeInfo);
+
+			if (pokemonData.description) {
+				let translatedDescription = pokemonData.description;
+				let translationType =
+					pokemonData.isLegendary || pokemonData.habitat == "cave"
+						? "yoda.json"
+						: "shakespeare.json";
+
+				translatedDescription = FunTranslatorApi.getFunTranslation(
+					translationType,
+					pokemonData.description
+				)
+					.then(() => {
+						pokemonData.description = translatedDescription;
+					})
+					.catch((translationError) => {
+						if (
+							translationError.response &&
+							translationError.response.status === 429
+						) {
+							console.warn(
+								"Translation API rate limit exceeded. Using standard description."
+							);
+						} else {
+							throw translationError;
+						}
+					});
+			}
+
+			res.json(pokemonData);
+		} catch (error) {
+			console.error(
+				`Error fetching or translating Pokemon description: ${error.message}`
+			);
+			res
+				.status(500)
+				.json({ error: "Failed to fetch or translate Pokemon description" });
+		}
 	},
 };
 
